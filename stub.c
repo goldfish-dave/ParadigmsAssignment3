6551fde14b04cf31a4276a9148df721f0107fc88
dwil <Wilcox; David dwil5124@ug.it.usyd.edu.au>
2011-10-10 22:03:48 +1100

    inital commit for dwil branch

---
+++ stub.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#define align(s,a) (((size_t)(s) + ((a) - 1)) & ~((size_t) (a) - 1))
+
+#define SSE_ALIGN (16)
+
+#define NUM (100)
+
+//extern void mymin(long, float *, float *, float *);
+
+int main (void) {
+	float *a = malloc(sizeof(float)*NUM + SSE_ALIGN),
+		  *b = malloc(sizeof(float)*NUM + SSE_ALIGN),
+		  *c = malloc(sizeof(float)*NUM + SSE_ALIGN);
+	int i;
+
+	a = (float *) align(a, SSE_ALIGN);
+	b = (float *) align(b, SSE_ALIGN);
+	c = (float *) align(c, SSE_ALIGN);
+
+	for (i = 0; i < NUM; i++) {
+		a[i] = i;
+		b[i] = NUM - i;
+	}
+
+	//mymin(NUM, a, b, c);
+
+	for (i = 0; i < NUM; i++) {
+		printf(c[i]);
+	}
+
+	return 0;
+}
