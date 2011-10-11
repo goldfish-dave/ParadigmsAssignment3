eb5749b1ad3343911b0a7962a0810ae6e75673e5
Ye; David <Ye; David daye4523@ug.it.usyd.edu.au>
2011-10-11 22:17:43 +1100

    comment out one block of operations to get the other one working

--- assemblyFile.s
+++ assemblyFile.s
@@ -19,6 +19,9 @@ imulq $4, %rax, %rax
 subq %rax, %rsp
 andq $-16, %rsp
 
+#create a constant, put in xmm2
+#leaq $.const<10.0>, %xmm2
+
 ### function body ###
 
 # load paramters
@@ -26,10 +29,9 @@ andq $-16, %rsp
 #movq %rsi, %r10
 #movq %rdx, %r11
 
-#create a constant, put in xmm2
-leaq $a, %xmm2
 
-# assign shit to an ident
+
+# assign shit to an ident, c = a
 movq %rsi, %rax
 movq %rcx, %r10
 
@@ -49,6 +51,33 @@ jnz loop_begin
 
 loop_end:
 
+# add shit together, c = c + a
+movq %rsi, %rax
+movq %rcx, %r10
+movq %rcx, %r11
+
+movq %rdi, %rbx
+shrq $2, %rbx
+jz loop_end
+
+loop_begin:
+movaps (%rax), %xmm0
+movaps (%r10), %xmm1
+
+#addition
+addps %xmm0, %xmm1
+
+movaps %xmm1, (%r11)
+
+#if rax == const etc.
+addq $16, %rax
+addq $16, %r11
+decq %rbx
+jnz loop_begin
+
+loop_end:
+
+
 # function epilogue
 popq	%rbx
 leave
@@ -56,8 +85,9 @@ ret
 
 .data
 .align 16
-.const <10>
-	.float 10
-	.float 10
-	.float 10
-	.float 10
+/*.const 10.0:
+	.float 10.0
+	.float 10.0
+	.float 10.0
+	.float 10.0
+*/
