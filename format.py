8c3b8b07877ab4521bc9a94e5b29b9972b809f95
Ye; David <Ye; David daye4523@ug.it.usyd.edu.au>
2011-10-13 00:35:48 +1100

    wrote up stubs for format.py

---
+++ format.py
@@ -0,0 +1,84 @@
+"""[
+	{	'declarations': ['x', 'y', 'z'],
+	 	'statements': [],
+	 	'parameters': ['a', 'b', 'c'],
+	 	'funcName': 'mymin'
+	}
+]
+
+
+	
+remember, statements are only of the form:
+
+IDENT = factor
+IDENT = factor op factor
+
+and in the simple case a factor is either a variable or a constant
+
+so expect statements to look like this:
+
+statements = 
+	[ 
+		( 'c' , 'b' ),
+		( 'x' , [ 'a', 'plus', 'c' ] ),
+		( 'a' , [ 'x', 'mult', 'b' ] ),
+		( 'y' , [ 'a', 'min' , 'x' ] )
+	]
+
+"""
+
+funcdef =	[
+				{
+					'declarations' : ['x', 'y', 'z']
+					, 'statements' : [('c', 20), ('c', ['c', 'plus', 'b'])]
+					, 'parameters' : ['a', 'b', 'c']
+					, 'funcName' : 'mymin'
+				}
+
+			]
+
+
+class registryMap:
+	def __init__(self, parameters, local):
+		self.mapping = {}
+
+		parameterRegisters = ['%rsi', '%rdx', '%rcx', '%r8', 'r9']
+		for parameter in range(len(parameters)):
+			self.mapping[parameters[parameter]] = "movq " + parameterRegisters[parameter] + ', %s' 
+
+			
+#		for var in local:
+				
+
+		print self.mapping
+
+def createLocals(localVars):
+	assignMemory = """
+#creating memory for local variables
+	blah %i
+	""".strip() % len(localVars)
+	
+	print assignMemory
+
+def createPreamble(name):
+
+	preamble = """
+#function definitions
+	%s %s %s
+	""" % (name, name, name)
+	print preamble
+
+def generateStatements(statements, regmap):
+	pass
+
+if __name__ == "__main__":
+	print funcdef
+	
+	for func in funcdef:
+		
+		createPreamble(func['funcName'])
+		createLocals(func['declarations'])
+		
+		regmap = registryMap(func['parameters'], func['declarations'])
+
+		generateStatements(func['statements'], regmap)
